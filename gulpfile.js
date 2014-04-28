4e3db54a5b0b8c0fc55a044f273ae3891efa6191
Jesse Wardle <jwardle@izeni.com>
2014-04-28 11:16:51 -0600

    Finished the compile task.

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,12 @@ var jshint = require('gulp-jshint');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
 var template = require('gulp-template');
 var karma = require('gulp-karma');
 var clean = require('gulp-clean');
-var using = require('gulp-using');
 var minifyCSS = require('gulp-minify-css');
-var help = require('gulp-task-listing');
-var streamqueue = require('streamqueue');
-
-// Add a task for the gulp-task-listing.
-gulp.task('help', help);
+var runSequence = require('run-sequence');
 
 // Include the config file.
 var config = require('./config.js');
@@ -28,6 +22,18 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
+// Clean the build directory.
+gulp.task('clean-build', function () {
+    return gulp.src(config.buildDir, {read: false})
+        .pipe(clean());
+});
+
+// Clean the compile directory.
+gulp.task('clean-compile', function () {
+    return gulp.src(config.compileDir, {read: false})
+        .pipe(clean());
+});
+
 // Copy all .js files maintaining relative path.
 gulp.task('build-js', function () {
     return gulp.src(config.appFiles.js, {base: './'})
@@ -35,7 +41,7 @@ gulp.task('build-js', function () {
 });
 
 // Copy all .less files and compile them while maintaining relative path.
-gulp.task('build-css', function () {
+gulp.task('build-less', function () {
     return gulp.src(config.appFiles.less, {base: './'})
         .pipe(less())
         .pipe(gulp.dest(config.buildDir));
@@ -48,7 +54,7 @@ gulp.task('build-html2js', function () {
         .pipe(gulp.dest(config.buildDir));
 });
 
-gulp.task('build-index', ['build-js', 'build-css', 'build-html2js'], function () {
+gulp.task('build-index', ['build-js', 'build-less', 'build-html2js'], function () {
     return gulp.src('src/index.html')
         .pipe(template({
             scripts: glob.sync('**/*.js', {
@@ -71,7 +77,7 @@ gulp.task('lint', function () {
 });
 
 // Concat and compile styles.
-gulp.task('compile-css', function () {
+gulp.task('compile-less', function () {
     return gulp.src(config.appFiles.less)
         .pipe(concat('main.min.less'))
         .pipe(less())
@@ -79,43 +85,58 @@ gulp.task('compile-css', function () {
         .pipe(gulp.dest(config.compileDir));
 });
 
+// Concatenate AngularJS templates into $templateCache.
+gulp.task('compile-html2js', function () {
+    return gulp.src(config.appFiles.appTpl)
+        .pipe(templateCache())
+        .pipe(gulp.dest(config.buildDir));
+});
+
 // Concatenate & minify js.
-gulp.task('compile-js', ['compile-html2js'], function () {
-    return gulp.src('bin/**/*.js')
+gulp.task('compile-js', function () {
+    return gulp.src(config.appFiles.js)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(config.buildDir));
+});
+
+gulp.task('compile-uglify', ['compile-html2js', 'compile-js'], function () {
+    return gulp.src(config.buildDir + '/*.js')
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.compileDir));
 });
 
 // Compile the index.
-gulp.task('compile-index', ['compile-js', 'compile-css'], function () {
+gulp.task('compile-index', ['compile-uglify', 'compile-less'], function () {
     return gulp.src('src/index.html')
         .pipe(template({
-            scripts: [config.compileDir + '/' + 'app.min.js'],
-            styles: [config.compileDir + '/' + 'main.css']
+            scripts: ['app.min.js'],
+            styles: ['main.min.css']
         }))
-        .pipe(gulp.dest(config.compileDir));
-});
-
-// Concatenate AngularJS templates into $templateCache.
-gulp.task('compile-html2js', function () {
-    return gulp.src(config.appFiles.appTpl)
-        .pipe(templateCache())
-        .pipe(uglify())
         .pipe(gulp.dest(config.compileDir));
 });
 
 // Watch files for changes.
 gulp.task('watch', function () {
-    gulp.watch(config.appFiles.js, ['lint', 'build-html2js', 'build-scripts']);
-    gulp.watch(config.appFiles.less, ['build-css']);
+    gulp.watch(config.appFiles.js, ['lint', 'build-html2js', 'build-js']);
+    gulp.watch(config.appFiles.less, ['build-less']);
 });
 
 // Development build task.
-gulp.task('build', [
-    'clean', 'lint', 'build-html2js', 'build-js', 'build-css', 'build-index',
-    'watch'
-]);
+gulp.task('build', function (callback) {
+    runSequence('clean', [
+        'lint', 'build-html2js', 'build-js', 'build-less', 'build-index',
+        'watch'],
+        callback
+    );
+});
 
 // Compile the files for production.
-gulp.task('compile', ['clean', 'compile-html2js', 'compile-js', 'compile-css']);
+gulp.task('compile', function (callback) {
+    runSequence('clean', [
+        'compile-less', 'compile-js', 'compile-html2js', 'compile-uglify',
+        'compile-index'],
+        'clean-build',
+        callback
+    );
+});
